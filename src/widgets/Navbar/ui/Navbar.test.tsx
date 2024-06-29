7ca583aa29b2ba1e891a14ecc9d61deb85fdd565
Valeria <8918valeria@gmail.com>
2024-06-29 22:07:30 +0300

    finally configured absolute paths in jest.config.ts

--- src/widgets/Navbar/ui/Navbar.test.tsx
+++ src/widgets/Navbar/ui/Navbar.test.tsx
@@ -1,25 +1,22 @@
+import { Navbar } from '@/widgets/Navbar'
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { MemoryRouter } from 'react-router-dom' // попробовать с браузер роутером // не помогло
-import { App } from '@/app/App'
+// import { App } from '@/app/App'
 
 describe('Navbar', () => {
     test('Router test', async () => {
         render(
             <MemoryRouter>
-                <App />
+                <Navbar />
             </MemoryRouter>
         )
         const user = userEvent.setup()
         screen.debug()
-        // verify page content for default route
-        expect(screen.getByText(/Sorry.. there was an error/i)).toBeInTheDocument()
-
-        // verify page content for expected route after navigating
-        await user.click(screen.getByTestId(/calculatorPage/i))
+        await user.click(screen.getByTestId('calculatorPage'))
+        expect(screen.getByTestId('calculator')).toBeInTheDocument()
         screen.debug()
-        expect(screen.getByTestId(/calculator/i)).toBeInTheDocument()
     })
 })
